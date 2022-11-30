fe1e6867b037ea29d187006f0d7a0810137d2f7c
Beden <+>
2022-11-30 22:11:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+//Manipulating our p element
+document.querySelector("p").textContent =
+  "JavaScript is so cool. It lets me add text to my page programmatically.";
+//creating DOM events
+const event = document.createEvent("Event");
+//Adding DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("p").textContent = "This is really cool!"; //Grabbing and reassigning value to our p element
+});
